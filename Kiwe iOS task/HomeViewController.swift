a83e30fdd59115a410c8191ac8936fad6f850f9a
Abdalla Shawky <shaaawkyy@gmail.com>
2020-10-05 18:39:56 +0200

    initializing project

---
+++ Kiwe iOS task/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiwe iOS task
+//
+//  Created by Abdalla Shawky on 10/5/20.
+//  Copyright © 2020 Abdalla Shawky. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
